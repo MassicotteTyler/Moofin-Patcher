dc1497a56630981e454c64a16d6558dafe6ada3d
massicottetyler <massicottetyler@gmail.com>
2016-03-21 19:42:57 -0700

    initial push

---
+++ MoofinPatcher/GUI.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace MoofinPatcher
+{
+    partial class GUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.fileButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(366, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // fileButton
+            // 
+            this.fileButton.Location = new System.Drawing.Point(385, 13);
+            this.fileButton.Name = "fileButton";
+            this.fileButton.Size = new System.Drawing.Size(23, 20);
+            this.fileButton.TabIndex = 1;
+            this.fileButton.Text = "...";
+            this.fileButton.UseVisualStyleBackColor = true;
+            this.fileButton.Click += new System.EventHandler(this.fileButton_Click);
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(13, 82);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(75, 23);
+            this.updateButton.TabIndex = 2;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(219, 82);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(187, 23);
+            this.progressBar.TabIndex = 3;
+            this.progressBar.Visible = false;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(216, 66);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(48, 13);
+            this.statusLabel.TabIndex = 4;
+            this.statusLabel.Text = "Progress";
+            this.statusLabel.Visible = false;
+            // 
+            // GUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 117);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.fileButton);
+            this.Controls.Add(this.textBox1);
+            this.Name = "GUI";
+            this.Text = "Moofin Patcher 0.2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button fileButton;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label statusLabel;
+    }
+}
+
---
+++ MoofinPatcher/GUI.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
+using System.Windows.Forms;
+
+namespace MoofinPatcher
+{
+    public partial class GUI : Form
+    {
+        private const string packLink = "https://www.dropbox.com/s/71idlrnv59mak6z/modz.zip?dl=1";
+
+        public GUI()
+        {
+            InitializeComponent();
+        }
+
+        private void updateButton_click(object sender, EventArgs e)
+        {
+            downloadFile();
+        }
+
+        private void fileButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void downloadFile()
+        {
+            using (WebClient wc = new WebClient())
+            {
+                statusLabel.Visible = true;
+                progressBar.Visible = true;
+                statusLabel.Text = "Downloading mods";
+                wc.DownloadProgressChanged += dl_DownloadProgressChanged;
+                wc.DownloadFileCompleted += downloadComplete;
+                wc.DownloadFileAsync(new Uri(packLink), "mods.zip");
+            }
+        }
+
+        private void extractFiles()
+        {
+            statusLabel.Text = "Extracting files";
+            try
+            {
+                ZipFile.ExtractToDirectory("mods.zip", textBox1.Text);
+
+            } catch (DirectoryNotFoundException e)
+            {
+                statusLabel.Text = "Output Directory not found";
+            }
+            catch (PathTooLongException e)
+            {
+                statusLabel.Text = "OutputPath too long";
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                statusLabel.Text = "Unauthorized access to output directory";
+            }
+            catch (Exception e)
+            {
+                statusLabel.Text = "Extracting failed";
+            }
+
+        }
+
+        private void dl_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            statusLabel.Text = string.Format("Download: {0} MB / {1} MB",
+                (e.BytesReceived / 1024d / 1024d).ToString("0.00"),
+                (e.TotalBytesToReceive / 1024d / 1024d).ToString("0.00"));
+            progressBar.Value = e.ProgressPercentage;
+        }
+
+        private void downloadComplete(object sender, AsyncCompletedEventArgs e)
+        {
+            statusLabel.Text = "Done";
+            extractFiles();
+
+        }
+    }
+}
